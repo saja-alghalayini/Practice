541dadf0ef5fdfa57e3eb8aa2f0b82766745685f
Saja Al Ghalayini <saja.ghalayini02@gmail.com>
2022-04-13 19:00:19 +0300

    prcts

--- HR Com. Emloyee/form.js
+++ HR Com. Emloyee/form.js
@@ -1,8 +1,11 @@
 
-'use strict';
+// 'use strict';
 let emplForm = document.getElementById("employeeForm");
-let tablebody = document.getElementById("bodyTa");
-// let name=document.getElementById('emplName');
+let email = document.getElementById("email");
+let emaDep = document.getElementById("emaDep");
+let tableHed = document.getElementById("tableHed");
+let tablebody = document.getElementById("tableBody");
+
 let arr = [];
 
 
@@ -17,26 +20,30 @@ function fromlocalstoarge ()
 {
     let keyy=localStorage.getItem('data');
     let parsekeyy=JSON.parse(keyy);
-    showdataontable(parsekeyy);
+    // showdataontable();
+    console.log(parsekeyy);
 
-    // let keyy = arr[arr.length-1].name ;
-    // let localstoargeitem = localStorage.getItem(keyy);
+    // let parsekeyy = arr[arr.length-1].name; //Ibrahim-z method
+    let localstoargeitem = localStorage.getItem(parsekeyy);
     
-    // let toObject = JSON.parse(localstoargeitem);
-    // if(toObject !== null)
-    // {
-    //     arr = toObject;
-    // }
+    let toObject = JSON.parse(localstoargeitem);
+    if(toObject !== null)
+    {
+        arr = toObject;
+    }
   
-    // return toObject;
+    return toObject;
+    showtable();
 }
 fromlocalstoarge();
+// console.log(localstoargeitem);
 
-
-function sale(fname , salary)
+function sale(fname , salary, email , emaDep)
 {
     this.fname = fname ; 
     this.salary = salary;
+    this.email = email;
+    this.emaDep = emaDep;
     arr.push(this);
     storeinlocalstorage()
 }
@@ -45,33 +52,80 @@ emplForm.addEventListener("submit", getdatafrominput)
 function getdatafrominput(event)
 {
  event.preventDefault();
+
  let fname = event.target.fname.value; 
  console.log(fname);
 
  let salary = Math.ceil(Math.random() * (500 - 100)+100);
  console.log(salary);
 
- new sale(fname,salary);
+ let email = event.target.email.value; 
+ console.log(email);
+
+ let emaDep = event.target.emaDep.value; 
+ console.log(emaDep);
+
+ new sale(fname,salary,email,emaDep);
+//  document.getElementById('name').innerHTML=localstoargeitem[0];
+//  document.getElementById('salary').innerHTML=localstoargeitem[1];
+//  document.getElementById('email').innerHTML=localstoargeitem[2];
+//  document.getElementById('emaDep').innerHTML=localstoargeitem[3];
  
 }
 
 getdatafrominput();
 
-function showdataontable()
-{
-    // let  getfromlocalstoarge = fromlocalstoarge();
-    // console.log(getfromlocalstoarge);
 
-    let tablerow = document.createElement("tr");
-    tablebody.appendChild(tablerow);
+function showtable(){
+for (let i =0; i< arr.length; i++){
+// let row=document.createElement("tr");
+// let d1=document.createElement("td");
+// let d2=document.createElement("td");
+// let d3=document.createElement("td");
+// let d4=document.createElement("td");
 
-    let tabledt1 = document.createElement("td");
-    tabledt1.innerText = arr[0].fname;
-    console.log(tabledt1);
+// tablebody.appendChild(row);
+// row.appendChild(d1);
+// row.appendChild(d2);
+// row.appendChild(d3);
+// row.appendChild(d4);
 
-    let tabledt2 = document.createElement("td");
-    tabledt2.innerText = arr[0].salary;
-    console.log(tabledt2);
-    
-    tablerow.append(tabledt1,tabledt2)
+let d1=document.getElementById("d1");
+let d2=document.getElementById("d2");
+let d3=document.getElementById("d3");
+let d4=document.getElementById("d4");
+
+
+d1.textContent=arr[0].fname;
+d2.textContent=arr[0].salary;
+d3.textContent=arr[0].email;
+d4.textContent=arr[0].emaDep;
+
 }
+}
+
+
+
+
+
+
+
+
+// function showdataontable()
+// {
+//     let  getfromlocalstoarge = fromlocalstoarge();
+//     console.log(getfromlocalstoarge());
+
+//     let tablerow = document.createElement("tr");
+//     tablebody.appendChild("td");
+
+//     let tabledt1 = document.createElement("td");
+//     tabledt1.innerText = arr[0].fname;
+//     console.log(tabledt1);
+
+//     let tabledt2 = document.createElement("td");
+//     tabledt2.innerText = arr[0].salary;
+//     console.log(tabledt2);
+    
+//     tablerow.append(tabledt1,tabledt2)
+let
